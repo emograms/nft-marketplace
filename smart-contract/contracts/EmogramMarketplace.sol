0ce3164170ef9ca211ba2e435bbbae710df0756b
DiodaPatr <robosaspien2@gmail.com>
2021-06-30 15:57:09 +0100

    Update EmogramMarketplace.sol

--- smart-contract/contracts/EmogramMarketplace.sol
+++ smart-contract/contracts/EmogramMarketplace.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
