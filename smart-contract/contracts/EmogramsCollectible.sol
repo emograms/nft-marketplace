6f2a43b81c711e18b145c856f4bdb257c4f7f36a
DiodaPatr <robosaspien2@gmail.com>
2021-09-13 17:04:33 +0100

    Fixed overflow in royalty calculation

--- smart-contract/contracts/EmogramsCollectible.sol
+++ smart-contract/contracts/EmogramsCollectible.sol
@@ -142,8 +142,13 @@ contract EmogramsCollectible is ERC1155, AccessControl, ERC1155Burnable {
     external
     view 
     returns (address receiver, uint256 royaltyAmount) {
-        
-        uint256 royaltyAmount = SafeMath.div(SafeMath.mul(_salePrice, BASE_PERCENTAGE), 10000);
+
+        if(_salePrice > 1e76) {
+                uint256 royaltyAmount = SafeMath.mul(SafeMath.div(_salePrice,10000),BASE_PERCENTAGE);
+        }
+        else {
+                uint256 royaltyAmount = SafeMath.div(SafeMath.mul(_salePrice, BASE_PERCENTAGE),10000);
+        }
 
         receiver = beneficiary;
 
